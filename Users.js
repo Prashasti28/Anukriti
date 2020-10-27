05b1a9221e39440225c6c0f305f1606c5e39bbe5
Prashasti28 <62647656+Prashasti28@users.noreply.github.com>
2020-10-27 14:53:25 +0530

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,64 @@
+const users = [
+  {
+    id : 1,
+    username: 'Shambhavi Kurup',
+    email: 'shambhavi@gmail.com',
+    password: 'skurup123'
+  },
+  {
+    id : "5f905ca9d20f2f354c0fd33c",
+    username: 'Prashasti Agarwal',
+    email: 'prashasti@gmail.com',
+    password: 'pagarwal123'
+  },
+  {
+    id :"5f905cd0d20f2f354c0fd33d",
+    username: 'Gauri Dasgupta',
+    email: 'gauri@gmail.com',
+    password: 'gdasgupta123'
+  },
+  {
+    id : "5f905d2dd20f2f354c0fd33e",
+    username: 'John Smith',
+    email: 'john@gmail.com',
+    password: 'jsmith123'
+  },
+  {
+    id: "5f905d49d20f2f354c0fd33f",
+    username: 'Seema Kapur',
+    email: 'seema@gmail.com',
+    password: 'skapur123'
+  },
+  {
+    id: "5f905d76d20f2f354c0fd340",
+    username: 'Hetal Shah',
+    email: 'hetal@gamil.com',
+    password: 'hshah123'
+  },
+  {
+    id: "5f905d93d20f2f354c0fd341",
+    username: 'Anirban Mondal',
+    email: 'anirban@gmail.com',
+    password: 'amondal123'
+  },
+  {
+    id: "5f905dafd20f2f354c0fd342",
+    username: 'Arjun Sharma',
+    email: 'arjun@gmial.com',
+    password: 'asharma123'
+  },
+  {
+    id: "5f905dd6d20f2f354c0fd343",
+    username: 'Rakhi Sen',
+    email: 'rakhi@gmail.com',
+    password: 'rsen123'
+  },
+  {
+    id: "5f905e0cd20f2f354c0fd344",
+    username: 'Arshia Mehta',
+    email: 'arshia@gmail.com',
+    password: 'amehta123'
+  }
+];
+
+module.exports = users;
